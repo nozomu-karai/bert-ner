6cd1511133614f94d63d014c1be8f5795de037d7
nozomu-karai <karai.nozomu.77c@st.kyoto-u.ac.jp>
2022-10-14 16:44:13 +0900

    クラスのBIOを記述しないでいいようにした

--- task.py
+++ task.py
@@ -1,12 +1,19 @@
 ner_label = {
-        'ud-japanese': ['O', 'B-PERSON', 'B-DATE', 'I-DATE', 'B-QUANTITY', 'I-QUANTITY', 'B-FAC', 'I-FAC', 'B-ORDINAL', 'I-ORDINAL', 'B-ORG', 'I-ORG', 'B-NORP', 'I-NORP', 'B-GPE', 'B-LOC', 'B-PRODUCT', 'I-PRODUCT', 'I-PERSON', 'B-TITLE_AFFIX', 'I-LOC', 'B-TIME', 'I-TIME', 'I-GPE', 'B-MONEY', 'I-MONEY', 'B-PERCENT', 'I-PERCENT', 'B-EVENT', 'I-EVENT', 'I-TITLE_AFFIX', 'B-WORK_OF_ART', 'I-WORK_OF_ART', 'B-MOVEMENT', 'I-MOVEMENT', 'B-LANGUAGE', 'B-LAW', 'I-LAW', 'I-LANGUAGE', 'B-CARDINAL', 'I-CARDINAL', 'B-PET_NAME', 'I-PET_NAME', 'B-PHONE', 'I-PHONE'],
+        'ud-japanese': ['O', 'PERSON', 'DATE', 'QUANTITY', 'FAC', 'ORDINAL', 'ORG', 'NORP', 'GPE', 'LOC', 'PRODUCT', 'TITLE_AFFIX', 'TIME', 'MONEY', 'PERCENT', 'EVENT', 'WORK_OF_ART', 'MOVEMENT', 'LANGUAGE', 'LAW', 'CARDINAL', 'PET_NAME', 'PHONE'],
         }
 
 
 def get_dict(task):
     ne_list = ner_label[task]
     ne_dict = {}
-    for i, ne in enumerate(ne_list):
-        ne_dict[ne] = i
+    i = 0
+    for ne in ne_list:
+        if ne == 'O':
+            ne_dict[ne] = i
+        else:
+            ne_ditc['B-'+ne] = i
+            i += 1
+            ne_dict['I-'+ne] = i
+        i += 1
 
     return ne_dict
